545ab03dbfe6ae58bd3cbeb5e3a4a8212443b1d5
dduarteingcom <daduarte@estudiantec.cr>
2023-04-17 08:49:45 -0600

    Carga de poderes en memoria

---
+++ InformationNode.h
@@ -0,0 +1,31 @@
+//
+// Created by dadu on 16/04/23.
+//
+
+#ifndef PROYECTO1DATOS2CE_CLIENTE_INFORMATIONNODE_H
+#define PROYECTO1DATOS2CE_CLIENTE_INFORMATIONNODE_H
+
+
+class InformationNode {
+public:
+    InformationNode(char nType, int data);
+
+    ~InformationNode();
+
+    char getType() const;
+
+    void setType(char type);
+
+    int getData() const;
+
+    void setData(int data);
+
+private:
+    char type;
+
+    int data;
+
+};
+
+
+#endif //PROYECTO1DATOS2CE_CLIENTE_INFORMATIONNODE_H
